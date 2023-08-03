ae3f0e79273aece7e4646d15410720068d0e877f
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-08-03 16:37:57 +0200

    done

--- models/student.model.js
+++ models/student.model.js
@@ -3,10 +3,20 @@ const mongoose = require("mongoose")
 // aqui vamos a crear el ESQUEMA
 // lo que da formato a los documentos de estudiantes. Dar validaciones.
 let studentSchema = new mongoose.Schema({
-  name: String,
-  candy: Number,
+  name: {
+    type: String,
+    required: true, // el campo es obligatorio
+    unique: true // evita duplicados con el mismo valor en la propiedad
+  },
+  candy: {
+    type: Number,
+    default: 1 // si no se agrega, esté será el valor predeterminado
+  },
   likesPokemon: Boolean,
-  pizzaToppings: [ String ]
+  pizzaToppings: [ {
+    type: String,
+    enum: ["piña", "peperoni", "tomate", "jamon"] // enum es los unicos posible valores aceptados
+  } ]
 })
 
 // ["pepperoni", "tomate", "piña"]
--- script.js
+++ script.js
@@ -7,22 +7,45 @@ const mongoose = require("mongoose");
 
 const Student = require("./models/student.model.js")
 
+const allStudents = require("./data/students.json")
+// json es data. todo archivo que sea data pura es automaticamente exportando
+
 mongoose.connect("mongodb://localhost:27017/students-db")
 .then((response) => {
   console.log("contados a la base de datos")
 
+
+  // (C) CREAR
   // hacer busquedas o modificaciones en la DB
-  return Student.create({
-    name: 1000,
-    candy: "patata",
-    likesPokemon: 1000,
-    pizzaToppings: {}
-  }) // crear un documento
+  // return Student.create({
+  //   name: "Angel",
+  //   pizzaToppings: ["anchoas"]
+  // }) // crear un documento
+
+  // return Student.insertMany(allStudents)
+
+  // (R) READ
+  // return Student
+  // .find( { candy: { $gte: 10 } } )
+  // .sort( { name: 1 } )
+  // .select( { name: 1, candy: 1 } ) // solo me devuelve propiedades de name y candy
+
+  // return Student.findOne({ likesPokemon: true })
+  // return Student.findById("64cbb0d1474c032d74d31c92")
+
+
+  // (U) UPDATE
+  // return Student.findOneAndUpdate({ name: "Mercedes" }, { likesPokemon: true, candy: 5 }, { new: true })
+  // mongoose de forma predenterminada nos devuelve el documento antes de hacer la actualizacion
+  // a veces queremos el document ACTUALIZADO. Para eso, debemos agregar un tercer argumento { new: true }
+  // return Student.findByIdAndUpdate("64cbb0d1474c032d74d31c92", { $push: { pizzaToppings: "piña" } })
+
+  // (D) DELETE
+  return Student.findOneAndDelete( {name: "Rubén"} )
+
 })
-.then(() => {
-  console.log("creado nuevo estudiante")
-
-
+.then((response) => {
+  console.log("la data es", response)
   return mongoose.connection.close() // cierra la conexion a la DB
 })
 .then(() => {
